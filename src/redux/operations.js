dffd3cfaea7710a95ac9c66dc00d62e4e7ae9d6d
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-16 20:33:26 +0100

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://63e4d3d0c04baebbcdace217.mockapi.io/contacts/';
+axios.defaults.baseURL =  'https://63e7a6a7cbdc5658737b0576.mockapi.io/api/v1';
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
@@ -9,8 +9,8 @@ export const fetchContacts = createAsyncThunk(
       try {
         const response = await axios.get('/contacts');
         return response.data;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error.message);
+      } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
       } 
     }
   );
@@ -20,8 +20,8 @@ export const fetchContacts = createAsyncThunk(
       try {
         const response = await axios.post('/contacts', newContact);
         return response.data;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error.message);
+      } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
       }
     }
   );
@@ -31,8 +31,8 @@ export const fetchContacts = createAsyncThunk(
       try {
         const response = await axios.delete(`/contacts/${contactId}`);
         return response.data;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error.message);
+      } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
       }
     }
   );
